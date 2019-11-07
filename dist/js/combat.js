c4f34674598828319f0e594661a18026c67e3609
Matthew Wessel <nucmatt@gmail.com>
2019-11-07 11:31:47 -0600

    added reciprocal shooting, next: make engagment loop shot delay agnostic

--- dist/js/combat.js
+++ dist/js/combat.js
@@ -1,26 +1,36 @@
-var airCombat = {
-  ufoHealth: 1000,
-  ufoHitChance: 50,
-  ufoShotDelay: 2000,
-  ufoEffDmg: 200,
-  xcomHealth: 1000,
-  xcomHitChance: 80,
-  xcomShotDelay: 2000,
-  xcomEffDmg: 200,
-  interceptionTime: 8000,
+// Constructors
 
-  shoot: function() {
-    if (this.shotChance() <= this.xcomHitChance) {
-      return this.rollDmg(this.xcomEffDmg, this.ufoHealth);
+// Objects
+let interceptor = {
+  health: 1000,
+  hitChance: 80,
+  shotDelay: 2000,
+  effDmg: 200
+};
+
+let ufo = {
+  health: 1000,
+  hitChance: 80,
+  shotDelay: 2000,
+  effDmg: 200
+};
+
+var airCombat = {
+  interceptionTime: 28000,
+
+  shoot: function(hitChance, effDmg) {
+    if (this.shotChance() <= hitChance) {
+      return this.rollDmg(effDmg);
     } else {
-      return console.log("miss");
+      console.log("miss");
+      return this.rollDmg(0);
     }
   },
 
-  rollDmg: function(effDmg, health) {
-    let shotDmg = Math.round(effDmg * (Math.random() * 0.5 + 1));
-    console.log(shotDmg);
-    return this.ufoHealth -= shotDmg;
+  rollDmg: function(effDmg) {
+    var shotDmg = Math.round(effDmg * (Math.random() * 0.5 + 1));
+    console.log("shot dmg " + shotDmg);
+    return shotDmg;
   },
 
   shotChance: function() {
@@ -28,17 +38,83 @@ var airCombat = {
   }
 };
 
-timer = Math.floor(Math.random() * 2000);
+// Air combat simulation
+// timerOne = Math.floor(Math.random() * 2000);
+// timerTwo = Math.floor(Math.random() * 2000);
 
-while (airCombat.ufoHealth > 0 && timer < airCombat.interceptionTime) {
-  console.log(timer);
-  console.log(airCombat.shoot());
-  timer += airCombat.xcomShotDelay;
-  console.log(timer);
+timerOne = 1500;
+timerTwo = 1000;
+
+// attempt 2 - label statements
+// xcomShoot:
+//     while (timerOne < airCombat.interceptionTime) {
+//         console.log("ufo health " + (ufo.health -= airCombat.shoot(interceptor.hitChance, interceptor.effDmg)));
+//         timerOne += interceptor.shotDelay;
+//     };
+
+// ufoShoot:
+//     while (timerTwo < airCombat.interceptionTime) {
+//         console.log("xcom health " + (interceptor.health -= airCombat.shoot(ufo.hitChance, ufo.effDmg)));
+//         timerTwo += ufo.shotDelay;
+//     }
+
+engagement: while (timerOne < timerTwo) {
+  console.log("xcom timer: " + timerOne + " ufo timer: " + timerTwo);
+  console.log(
+    "ufo health " +
+      (ufo.health -= airCombat.shoot(interceptor.hitChance, interceptor.effDmg))
+  );
+  timerOne += interceptor.shotDelay;
+  if (ufo.health <= 0) {
+    break engagement;
+  }
+  while (timerTwo < timerOne) {
+    console.log("xcom timer: " + timerOne + " ufo timer: " + timerTwo);
+    console.log(
+      "xcom health " +
+        (interceptor.health -= airCombat.shoot(ufo.hitChance, ufo.effDmg))
+    );
+    timerTwo += ufo.shotDelay;
+    if (interceptor.health <= 0) {
+      break engagement;
+    }
+  }
+  if ((timerOne && timerTwo) > airCombat.interceptionTime) {
+    break;
+  }
 }
-// console.log(airCombat.shoot());
-// console.log(airCombat.rollDmg());
-// console.log(airCombat.ufoHealth)
-// console.log(airCombat.shoot());
-// console.log(airCombat.rollDmg());
-// console.log(airCombat.ufoHealth);
+
+// // attempt 1 - pure while loop
+// while (((timerOne && timerTwo) < airCombat.interceptionTime) && (ufo.health || interceptor.health) > 0)  {
+//   console.log("xcom timer " + timerOne);
+//   console.log("ufo timer " + timerTwo);
+//   if (timerOne < timerTwo) {
+//     console.log(
+//       "ufo health " +
+//         (ufo.health -= airCombat.shoot(
+//           interceptor.hitChance,
+//           interceptor.effDmg
+//         ))
+//     );
+//     timerOne += interceptor.shotDelay;
+//     console.log(
+//       "xcom health " +
+//         (interceptor.health -= airCombat.shoot(ufo.hitChance, ufo.effDmg))
+//     );
+//     timerTwo += ufo.shotDelay;
+//   } else {
+//     console.log(
+//       "xcom health " +
+//         (interceptor.health -= airCombat.shoot(ufo.hitChance, ufo.effDmg))
+//     );
+//     timerTwo += ufo.shotDelay;
+//     console.log(
+//       "ufo.health " +
+//         (ufo.health -= airCombat.shoot(
+//           interceptor.hitChance,
+//           interceptor.effDmg
+//         ))
+//     );
+//     timerOne += interceptor.shotDelay;
+//   }
+// }
